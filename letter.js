153114575f00c57ac4fa23c3aebd7a067a2a8a54
noracyrd <norabyrd@alumni.upenn.edu>
2019-10-04 17:11:13 -0500

    comments for Letter constructor

--- letter.js
+++ letter.js
@@ -1,14 +1,20 @@
+// Constructor for each letter of the mystery word!
+
 function Letter(character) {
     this.character = character;
+    // setting initial state - no one's guessed it yet, so guessed is false
     this.guessed = false;
     this.isGuessed = function(){
+            // once the letter's been guessed, the guessed flag is true, and we return the underlying character
         if (this.guessed){
             return this.character;
+        // otherwise we'll show the placeholder underscore
         } else {
             return "_ ";
         }
     }
     this.userGuess = function(guess){
+        // when the user guesses the right letter, we swap the guessed flag and display the hidden letter
         if (guess === this.character){
             this.guessed = true;
             this.isGuessed();
