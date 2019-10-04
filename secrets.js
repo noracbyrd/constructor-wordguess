9011c8e6dfedfa51abd5d9c50832e0c15be9d306
noracyrd <norabyrd@alumni.upenn.edu>
2019-10-04 17:54:23 -0500

    added some secret words and added comments for the secret words

--- secrets.js
+++ secrets.js
@@ -1,9 +1,11 @@
+// our possible mystery words!
 var secret = {
-    words: ["TANGLED","FROZEN","CINDERELLA","MOANA","POCAHONTAS","SNOW WHITE","BEAUTY AND THE BEAST", "THE LITTLE MERMAID"]
+    words: ["TANGLED","FROZEN","CINDERELLA","MOANA","POCAHONTAS","SNOW WHITE AND THE SEVEN DWARFS","BEAUTY AND THE BEAST","THE LITTLE MERMAID","SLEEPING BEAUTY","THE PRINCESS AND THE FROG","OLIVER AND COMPANY","THE BLACK CAULDRON","FANTASIA","ENCHANTED","LADY AND THE TRAMP","ONE HUNDRED ONE DALMATIONS","THE LION KING","THE ARISTOCATS","BAMBI","THE FOX AND THE HOUND","ROBIN HOOD","THE JUNGLE BOOK","THE RESCUERS","PETES DRAGON","THE GREAT MOUSE DETECTIVE","LILO AND STITCH"]
 }
 
+// function to choose a random word from the above array:
 var randomWord = function(){
-    return secret.words[Math.floor(Math.random()*6)];
+    return secret.words[Math.floor(Math.random()*25)];
 }
 
 
